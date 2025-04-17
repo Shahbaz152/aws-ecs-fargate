5f205d155d2ebda9920c6c40dbcad2bde0e9e8b4
Shahbaz152 <65075028+Shahbaz152@users.noreply.github.com>
2025-04-18 00:19:25 +0530

    Delete vuln.py

--- vuln.py
+++
@@ -1,9 +0,0 @@
-# 🚨 Deliberate SonarCloud issues
-import os
-import sys  # unused
-
-password = "mysecret"  # hardcoded
-os.system("rm -rf /")  # dangerous command
-
-def never_called():
-    print("I'm never called!")
