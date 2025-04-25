7c4e1ad2934170b9a6ab6fe59d80acc77d286e79
Shahbaz152 <65075028+Shahbaz152@users.noreply.github.com>
2025-04-25 22:37:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is my python web application with ECS'
+    return 'Hello, World! This is my python web application with ECS Blue=Green'
 
 @app.route('/health')
 def health():
